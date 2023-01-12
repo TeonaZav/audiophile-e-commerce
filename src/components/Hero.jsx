8a6e547a6e4e80fb74bb673f7bf359a3258cbf05
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-12 11:13:50 +0400

    style: fix hero section styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,18 +5,20 @@ function Hero(props) {
   return (
     <Wrapper>
       <section className="hero">
-        <div className="hero-text-box">
-          <p className="sub-heading">NEW PRODUCT</p>
-          <h1>XX99 Mark II HeadphoneS</h1>
-          <p>
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </p>
-        </div>
-        <div>
-          <NavLink className="btn btn--full" to="/">
-            See Product
-          </NavLink>
+        <div className="hero-ct">
+          <div className="hero-text-box">
+            <p className="sub-heading">NEW PRODUCT</p>
+            <h1>XX99 Mark II HeadphoneS</h1>
+            <p>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+          </div>
+          <div>
+            <NavLink className="btn btn--full" to="/">
+              See Product
+            </NavLink>
+          </div>
         </div>
       </section>
     </Wrapper>
@@ -34,7 +36,16 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+
+  .hero-ct {
+    height: 100%;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
   .hero-text-box {
     width: 32.8rem;
@@ -81,10 +92,35 @@ const Wrapper = styled.div`
   @media (min-width: 48em) {
     height: 64rem;
     background-image: url(${require(`.././assets/home/tablet/image-header.jpg`)});
+    .hero-text-box {
+      width: 39.6rem;
+      margin-bottom: 4rem;
+    }
+
+    h1 {
+      font-weight: 700;
+      font-size: 5.6rem;
+      line-height: 5.8rem;
+      letter-spacing: 0.2rem;
+    }
   }
   @media (min-width: 90em) {
     height: 64rem;
     background-image: url(${require(`.././assets/home/desktop/image-hero.jpg`)});
+    .hero-ct {
+      width: 77%;
+      justify-content: center;
+      align-items: flex-start;
+      margin: 0 auto;
+    }
+    .hero-text-box {
+      width: 39.6rem;
+      margin-bottom: 4rem;
+      text-align: left;
+    }
+    .sub-heading {
+      align-self: flex-start;
+    }
   }
 `;
 export default Hero;
