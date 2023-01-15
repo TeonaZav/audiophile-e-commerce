b1d9f7a51e01999d29b1f51f6cd1ab6f0409f896
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-15 14:24:51 +0400

    feat: fetch products data from database

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,14 +1,24 @@
-import React, { useContext, useReducer } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useReducer } from "react";
 import reducer from "../reducers/products_reducer";
 
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  PRODUCTS_FETCH_START,
+  PRODUCTS_FETCH_SUCCESS,
+  PRODUCTS_FETCH_ERROR,
+} from "../actions";
 
 const initialState = {
   menuIsClose: true,
+  productIsLoading: false,
+  products: [],
+  productsFetchError: false,
 };
 
 const ProductsContext = React.createContext();
-
+const url = "http://localhost:5000/api/v1/products/all";
 export const ProductsProvider = ({ children }) => {
   const [state, dispach] = useReducer(reducer, initialState);
   const openSidebar = () => {
@@ -17,7 +27,23 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispach({ type: SIDEBAR_CLOSE });
   };
-
+  /*================
+    get products data 
+   =================*/
+  const getProducts = async (url) => {
+    dispach({ type: PRODUCTS_FETCH_START });
+    try {
+      const res = await axios.get(url);
+      const products = res.data;
+      dispach({ type: PRODUCTS_FETCH_SUCCESS, payloads: products });
+      console.log(products.data);
+    } catch (error) {
+      dispach({ type: PRODUCTS_FETCH_ERROR });
+    }
+  };
+  useEffect(() => {
+    getProducts(url);
+  }, []);
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,4 +1,10 @@
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  PRODUCTS_FETCH_START,
+  PRODUCTS_FETCH_SUCCESS,
+  PRODUCTS_FETCH_ERROR,
+} from "../actions";
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
@@ -8,6 +14,21 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     console.log({ ...state, menuIsClose: false });
     return { ...state, menuIsClose: false };
+  }
+  if (action.type === PRODUCTS_FETCH_START) {
+    console.log({ ...state, productIsLoading: true });
+    return { ...state, productIsLoading: true };
+  }
+  if (action.type === PRODUCTS_FETCH_SUCCESS) {
+    console.log({
+      ...state,
+      products: action.payload,
+      productIsLoading: false,
+    });
+    return { ...state, products: action.payload, productIsLoading: false };
+  }
+  if (action.type === PRODUCTS_FETCH_ERROR) {
+    return { ...state, productIsLoading: false, productsFetchError: true };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
