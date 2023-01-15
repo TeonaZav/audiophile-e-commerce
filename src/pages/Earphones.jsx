f39c701b2a91ed5629527acbafa05a9fe76a878d
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-15 04:24:06 +0400

    pass data to ProductsLists

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -3,10 +3,14 @@ import Header from "../components/sharedComponents/Header";
 import CategoryItems from "../components/sharedComponents/CategoryItems";
 import About from "../components/About";
 import Footer from "../components/sharedComponents/Footer";
+import ProductList from "../components/ProductList";
+import data from "../data.json";
 function Earphones(props) {
+  const earphonesData = data.filter((el) => el.category === "earphones");
   return (
     <div>
       <Header />
+      <ProductList data={earphonesData} />
       <CategoryItems />
       <About />
       <Footer />
--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -3,10 +3,14 @@ import Header from "../components/sharedComponents/Header";
 import CategoryItems from "../components/sharedComponents/CategoryItems";
 import About from "../components/About";
 import Footer from "../components/sharedComponents/Footer";
+import ProductList from "../components/ProductList";
+import data from "../data.json";
+const speakerssData = data.filter((el) => el.category === "speakers");
 function Speakers() {
   return (
     <div>
       <Header />
+      <ProductList data={speakerssData} />
       <CategoryItems />
       <About />
       <Footer />
