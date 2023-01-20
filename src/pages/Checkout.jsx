3c53a9f68ec844781c0916b2840eb844b92ca0a9
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-20 12:51:47 +0400

    add header and footer to checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import CheckoutForm from "../components/CheckoutForm";
+import Header from "../components/sharedComponents/Header";
+import Footer from "../components/sharedComponents/Footer";
+
 function Checkout(props) {
   return (
     <div>
+      <Header />
       <CheckoutForm />
+      <Footer />
     </div>
   );
 }
